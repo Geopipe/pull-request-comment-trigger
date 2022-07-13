9c651339b16b4eb88134a3ad326347c4e45241fd
Thomas Dickerson <elfprince13@gmail.com>
2022-07-13 14:28:48 -0400

    Also respond to github pull request review comments (#3)
    For reasons that are somewhat mysterious to me, comment runs sometimes appear to get correctly associated with commits and sometimes do not. Calling this good enough for now.
    
    * Also respond to github pull request review comments
    
    * Update branch to use for running actions
    
    * Test associating a check run with the recent PR commit, even if it's from a comment
    
    * add debug output
    
    * Improved debug logging
    
    * Forgot to unwrap PR response from HTTP surroundings
    
    * Try to fix workflow syntax
    
    * Once more unto the breach
    
    * fix missing mergable attribute
    
    * Fix reactions on pull request review comments

--- index.js
+++ index.js
@@ -10,17 +10,19 @@ async function run() {
     const trigger = core.getInput("trigger", { required: true });
     // testing note: set by environment variable INPUT_REACTION
     const reaction = core.getInput("reaction");
+    // testing note: set by environment variable INPUT_FAIL_IF_NOT_MERGEABLE
+    const failUnmergeable = core.getInput("fail_if_not_mergeable")
     const { GITHUB_TOKEN } = process.env;
     if (reaction && !GITHUB_TOKEN) {
         core.setFailed('If "reaction" is supplied, GITHUB_TOKEN is required');
         return;
-    } else if (context.eventName !== "issue_comment" && context.eventName !== "pull_request") {
-        core.setFailed("eventName must be issue_comment or pull_request");
+    } else if (context.eventName !== "issue_comment" && context.eventName !== "pull_request" && context.eventName !== "pull_request_review_comment") {
+        core.setFailed("eventName must be issue_comment or pull_request or pull_request_review_comment");
         return;
     }
 
     const body =
-        (context.eventName === "issue_comment"
+        ((context.eventName === "issue_comment" || context.eventName === "pull_request_review_comment")
         // For comments on pull requests
             ? context.payload.comment.body
             // For the initial pull request description
@@ -35,25 +37,54 @@ async function run() {
         if (context.payload.issue.pull_request) {
             const pull_url = context.payload.issue.pull_request.url;
             const pull_number_index = pull_url.lastIndexOf('/')
-            if(-1 === pull_number_index) {
+            if (-1 === pull_number_index) {
                 core.setFailed("Invalid pull request URL extracted from issue_comment")
                 return;
             } else {
                 const pull_number = parseInt(pull_url.substring(1 + pull_number_index));
-                core.setOutput("pull_request", await client.pulls.get({
+                const pull_response = await client.pulls.get({
                     owner,
                     repo,
                     pull_number,
-                }));
+                });
+
+                if (pull_response.status == 200) {
+                    context.payload.pull_request = pull_response.data;
+                } else {
+                    core.setFailed("Failed to obtain response from server");
+                    return
+                }
             }
         } else {
             // not a pull-request comment, aborting
             core.setOutput("triggered", "false");
             return;
         }
-    } else {
-        core.setOutput("pull_request", context.payload.pull_request);
+    } else if (failUnmergeable) {
+        // For whatever reason the PR objects embedded in pull_request and pull_request_review_comment payloads are incomplete
+        // in particular - they don't have the mergeable field we need to implement failUnmergeable, so we will extract their number
+        // and then poll that endpoint directly
+        const pull_number = context.payload.pull_request.number;
+        const pull_response = await client.pulls.get({
+            owner,
+            repo,
+            pull_number,
+        });
+
+        if (pull_response.status == 200) {
+            context.payload.pull_request = pull_response.data;
+        } else {
+            core.setFailed("Failed to obtain response from server");
+            return
+        }
     }
+
+    core.setOutput("pull_request", context.payload.pull_request);
+    if(failUnmergeable && context.payload.pull_request.mergeable != true) {
+        core.setFailed("The pull request was not in a mergeable state, expected 'true' and got " + context.payload.pull_request.mergeable);
+        return;
+    }
+    
 
     const prefixOnly = core.getInput("prefix_only") === 'true';
     if ((prefixOnly && !body.startsWith(trigger)) || !body.includes(trigger)) {
@@ -74,11 +105,18 @@ async function run() {
             comment_id: context.payload.comment.id,
             content: reaction
         });
-    } else {
+    } else if (context.eventName === "pull_request") {
         await client.reactions.createForIssue({
             owner,
             repo,
             issue_number: context.payload.pull_request.number,
+            content: reaction
+        });
+    } else if (context.eventName === "pull_request_review_comment") {
+        await client.reactions.createForPullRequestReviewComment({
+            owner,
+            repo,
+            comment_id: context.payload.comment.id,
             content: reaction
         });
     }
